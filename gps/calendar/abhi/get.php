4f06286a74989479307594070bca324a53fd06ae
csljaipur <33312039+csljaipur@users.noreply.github.com>
2017-11-02 15:30:00 +0530

    Add files via upload

---
+++ gps/calendar/abhi/get.php
@@ -0,0 +1,15 @@
+<?php 
+$datefrom=$_POST['date1'];
+$dateto=$_POST['date2'];
+//$string1="00:00:00";
+//$from='\''.$datefrom."&nbsp;".$string1.'\'';
+//$to='\''.$dateto."&nbsp;".$string1.'\'';
+echo "<b><br>Selected Records in between Date & Time from " .$datefrom. " to " .$dateto. '</b>';
+?>
+
+
+
+
+		
+	
+	
